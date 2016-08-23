41cd9c6c127a328c4e994c4c8bf458a4ce9aedb7
bamtoll09 <ryan345@nate.com>
2016-08-24 02:39:23 +0900

    first commit

---
+++ Assets/Script/CameraMagager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMagager : MonoBehaviour {
+
+    float speed = 60.0f;
+    float time = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (time >= 1.2f && time < 1.95f)
+        {
+            if (transform.rotation.x > -45)
+                transform.Rotate(new Vector3(-speed, 0.0f, 0.0f) * Time.deltaTime);
+        }
+        else if (speed == 45.0f)
+        {
+            speed = 30.0f;
+        }
+        else if (time >= 2.2f && time < 3.7f)
+        {
+            if (transform.rotation.x > 0.0f)
+                transform.Rotate(new Vector3(speed, 0.0f, 0.0f) * Time.deltaTime);
+        }
+        else if (time >= 3.7f && transform.rotation.x != 0.0f)
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        time += Time.deltaTime;
+	}
+
+    void OnDisabled()
+    {
+        time = 0.0f;
+        speed = 60.0f;
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject box_left;
+    public GameObject box_right;
+    public GameObject stick_left;
+    public GameObject stick_right;
+    public GameObject particle_top;
+    public GameObject particle_left;
+    public GameObject particle_right;
+    //public GameObject circleBoard;
+    //public GameObject CMR;
+
+    public Animator ani_box_left;
+    public Animator ani_box_right;
+    public Animator ani_stick_left;
+    public Animator ani_stick_right;
+    public Animator ani_circleBoard;
+    public Animator ani_cmr;
+
+    public CircleBoard circleBoard;
+    //public CameraMagager cmr;
+    public Text resultText;
+    public Button nextBtn;
+
+    public static GameManager instance;
+
+    bool pressed = false;
+    float time = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        instance = this;
+        circleBoard.enabled = false;
+        //cmr.enabled = false;
+        resultText.text = "";
+        nextBtn.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (pressed)
+        {
+            if (time >= 4.0f)
+            {
+                resultText.text = "황승연 1일 이용권";
+                nextBtn.enabled = true;
+            }
+
+            time += Time.deltaTime;
+        }
+
+	}
+
+    public void openRootBox()
+    {
+        particle_top.SetActive(false);
+        particle_left.SetActive(false);
+        particle_right.SetActive(false);
+
+        ani_box_left.SetBool("isClicked", true);
+        ani_box_right.SetBool("isClicked", true);
+        ani_stick_left.SetBool("isClicked", true);
+        ani_stick_right.SetBool("isClicked", true);
+        ani_circleBoard.SetBool("isClicked", true);
+        ani_cmr.SetBool("isClicked", true);
+
+        circleBoard.enabled = true;
+        //cmr.enabled = true;
+
+        pressed = true;
+    }
+
+    public void reset() // Next
+    {
+
+    }
+}
